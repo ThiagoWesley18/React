8041a54fdb9576dad4d0d53be539211365ba5621
ThiagoWesley18 <tw180896@gmail.com>
2024-05-24 11:14:24 -0400

    Listagens de Carrinho e produto

--- app-loja/src/app/carrinho/page.tsx
+++ app-loja/src/app/carrinho/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ListagemCarrinho from "../components/ListagemCarrinho";
 
 export default function Carrinho() {
-  const valorTotalProduto = (
-    precoUnitario: number,
-    quantidade: number
-  ): number => precoUnitario * quantidade;
-
   return (
     <>
       <main>
@@ -17,59 +13,8 @@ export default function Carrinho() {
               <h5 className="card-title mb-4 fw-light">
                 Produtos selecionados
               </h5>
-              <div className="table-responsive">
-                <table className="table ">
-                  <thead>
-                    <tr>
-                      <th>Produto</th>
-                      <th>Valor Unitário</th>
-                      <th>Quantidade</th>
-                      <th>Valor Total</th>
-                      <th>Opções</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr key="1">
-                      <td>Notebook 1</td>
-                      <td>R$ {(1500).toFixed(2)}</td>
-                      <td>2</td>
-
-                      <td>R$ {valorTotalProduto(1500, 2).toFixed(2)}</td>
-                      <td>
-                        <button className="btn btn-danger btn-sm">
-                          Remover
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr key="1">
-                      <td>Notebook 2 </td>
-                      <td>R$ {(1500).toFixed(2)}</td>
-                      <td>2</td>
-
-                      <td>R$ {valorTotalProduto(1500, 2).toFixed(2)}</td>
-                      <td>
-                        <button className="btn btn-danger btn-sm">
-                          Remover
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr key="1">
-                      <td>Notebook 3</td>
-                      <td>R$ {(1500).toFixed(2)}</td>
-                      <td>3</td>
-
-                      <td>R$ {valorTotalProduto(1500, 3).toFixed(2)}</td>
-                      <td>
-                        <button className="btn btn-danger btn-sm">
-                          Remover
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <ListagemCarrinho />
+              
             </div>
           </div>
 
---
+++ app-loja/src/app/components/ListagemCarrinho.tsx
@@ -0,0 +1,61 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function ListagemCarrinho() {
+    const valorTotalProduto = (precoUnitario: number, quantidade: number): number => precoUnitario * quantidade;
+    
+    return (
+        <div className="table-responsive">
+            <table className="table ">
+                <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Valor Unitário</th>
+                    <th>Quantidade</th>
+                    <th>Valor Total</th>
+                    <th>Opções</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr key="1">
+                    <td>Notebook 1</td>
+                    <td>R$ {(1500).toFixed(2)}</td>
+                    <td>2</td>
+
+                    <td>R$ {valorTotalProduto(1500, 2).toFixed(2)}</td>
+                    <td>
+                    <button className="btn btn-danger btn-sm">
+                        Remover
+                    </button>
+                    </td>
+                </tr>
+
+                <tr key="1">
+                    <td>Notebook 2 </td>
+                    <td>R$ {(1500).toFixed(2)}</td>
+                    <td>2</td>
+
+                    <td>R$ {valorTotalProduto(1500, 2).toFixed(2)}</td>
+                    <td>
+                    <button className="btn btn-danger btn-sm">
+                        Remover
+                    </button>
+                    </td>
+                </tr>
+
+                <tr key="1">
+                    <td>Notebook 3</td>
+                    <td>R$ {(1500).toFixed(2)}</td>
+                    <td>3</td>
+
+                    <td>R$ {valorTotalProduto(1500, 3).toFixed(2)}</td>
+                    <td>
+                    <button className="btn btn-danger btn-sm">
+                        Remover
+                    </button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );    
+}
